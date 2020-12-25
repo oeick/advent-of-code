8fa06e3b407302069bf12e893b07b983974080af
oeick <oliver.eickmeyer@gmail.com>
2020-12-25 17:40:58 +0100

    solutions day 03

---
+++ 2020/03/main.py
@@ -0,0 +1,26 @@
+with open('input.txt', 'r') as input_file:
+    lines = [line.strip() for line in input_file.readlines()]
+
+prod = 1
+for slope in [1, 3, 5, 7]:
+    trees = 0
+    col = 0
+    for row in lines:
+        if row[col] == '#':
+            trees += 1
+        col += slope
+        col = col % len(row)
+    prod *= trees
+    if slope == 3:
+        print(trees)
+
+trees = 0
+col = 0
+for i_row in range(0, len(lines), 2):
+    if lines[i_row][col] == '#':
+        trees += 1
+    col += 1
+    col = col % len(lines[0])
+
+prod *= trees
+print(prod)
