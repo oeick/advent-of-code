5b61e4a15cbb34db848732ca1c088c26d65f8f4e
oeick <oliver.eickmeyer@gmail.com>
2020-12-31 12:16:24 +0100

    added Rust solution day 3 part 2

--- 2020/03/rust/src/main.rs
+++ 2020/03/rust/src/main.rs
@@ -2,13 +2,27 @@ use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>>{
     let input = std::fs::read_to_string(r"..\input.txt")?;
-    const SLOPE: usize = 3;
-    let counter: usize = input
-        .lines()
-        .enumerate()
-        .filter(|(i, row)| row.chars()
-                              .nth(i * SLOPE % row.len()) == Some('#'))
-        .count();
-    println!("{}", counter);
+    println!("{}", solve_part1(&input));
+    println!("{}", solve_part2(&input));
     Ok(())
+}
+
+fn solve_part1(input: &String) -> usize{
+    count_trees(&input, 3, 1)
+}
+
+fn solve_part2(input: &String) -> usize{
+    let slopes = vec!((1, 1), (3, 1), (5, 1), (7, 1), (1, 2));
+    slopes.iter()
+          .map(|&(right, down)| count_trees(&input, right, down))
+          .fold(1, |p, x| p * x)
+}
+
+fn count_trees(input: &String, right: usize, down: usize) -> usize{
+    input.lines()
+         .step_by(down)
+         .enumerate()
+         .filter(|(i, row)| row.chars()
+                               .nth(i * right % row.len()) == Some('#'))
+         .count()
 }
