1e1238b2d12faa2f4d40c6e726567b48e6d8a5f8
oeick <oliver.eickmeyer@gmail.com>
2020-12-31 11:14:43 +0100

    introduced const in Rust solution day 3 part 1

--- 2020/03/rust/src/main.rs
+++ 2020/03/rust/src/main.rs
@@ -1,19 +1,13 @@
 use std::error::Error;
 
 fn main() -> Result<(), Box<dyn Error>>{
-    let input_file = std::fs::read_to_string(r"..\input.txt")?;
-    // let mut col = 0;
-    // let mut counter = 0;
-    // for line in input_file.lines(){
-    //     if line.chars().nth(col) == Some('#'){
-    //         counter += 1;
-    //     }
-    //     col = (col + 3) % line.len();
-    // }
-    let counter: usize = input_file
+    let input = std::fs::read_to_string(r"..\input.txt")?;
+    const SLOPE: usize = 3;
+    let counter: usize = input
         .lines()
         .enumerate()
-        .filter(|(i, line)| line.chars().nth(i*3 % line.len()) == Some('#') )
+        .filter(|(i, row)| row.chars()
+                              .nth(i * SLOPE % row.len()) == Some('#'))
         .count();
     println!("{}", counter);
     Ok(())
