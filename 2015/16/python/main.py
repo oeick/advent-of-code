0bb0eb53046e0c272449b22ab15acbf89f6626bc
oeick <oliver.eickmeyer@gmail.com>
2021-02-28 17:58:32 +0100

    added Python solution for 2015 day 16 part 2

--- 2015/16/python/main.py
+++ 2015/16/python/main.py
@@ -17,22 +17,42 @@ MFCSAM_OUTPUT = [
     'perfumes: 1']
 
 
-def solve_part_1(lines: list[str]) -> int:
-    for line in lines:
-        number = NUMBER_PATTERN.search(line).group(1)
-        compounds = COMPOUND_PATTERN.findall(line)
+def solve_part_1(sues: dict[str, list]) -> int:
+    for number, compounds in sues.items():
         if all(c in MFCSAM_OUTPUT for c in compounds):
             return int(number)
 
 
-def solve(lines: list[str]) -> (int, int):
-    return solve_part_1(lines), 0
+def solve_part_2(sues: dict[str, list]) -> int:
+    mfcsam_dict = {c.split()[0]: int(c.split()[1]) for c in MFCSAM_OUTPUT}
+    for number, compounds in sues.items():
+        valid = True
+        for compound in compounds:
+            kind, amount_str = compound.split()
+            amount = int(amount_str)
+            if kind in ['cats:', 'trees:']:
+                if amount <= mfcsam_dict[kind]:
+                    valid = False
+                    continue
+            elif kind in ['pomeranians:', 'goldfish:']:
+                if amount >= mfcsam_dict[kind]:
+                    valid = False
+                    continue
+            else:
+                if amount != mfcsam_dict[kind]:
+                    valid = False
+                    continue
+        if valid:
+            return int(number)
 
 
 def main(filename: str) -> (int, int):
     with open(filename, 'r') as fp:
         lines = fp.readlines()
-    return solve(lines)
+    sues = {
+        NUMBER_PATTERN.search(line).group(1): COMPOUND_PATTERN.findall(line)
+        for line in lines}
+    return solve_part_1(sues), solve_part_2(sues)
 
 
 if __name__ == '__main__':
--- 2015/16/python/tests/test_examples.py
+++ 2015/16/python/tests/test_examples.py
@@ -5,10 +5,18 @@ import main
 
 class ExampleTests(unittest.TestCase):
 
-    def test_example(self):
+    def test_part_1(self):
         result = main.solve_part_1(
-            ['Sue 1: trees: 7, perfumes: 2, akitas: 5',
-             'Sue 2: children: 3, goldfish: 4, vizslas: 0',
-             'Sue 3: children: 3, goldfish: 5, vizslas: 0']
+            {'1': ['trees: 7', 'perfumes: 2', 'akitas: 5'],
+             '2': ['children: 3', 'goldfish: 4', 'vizslas: 0'],
+             '3': ['children: 3', 'goldfish: 5', 'vizslas: 0']}
+        )
+        self.assertEqual(3, result)
+
+    def test_part_2(self):
+        result = main.solve_part_2(
+            {'1': ['perfumes: 1', 'trees: 2', 'goldfish: 0'],
+             '2': ['perfumes: 1', 'trees: 6', 'goldfish: 5'],
+             '3': ['perfumes: 1', 'trees: 6', 'goldfish: 0']}
         )
         self.assertEqual(3, result)
