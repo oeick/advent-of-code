702cdedbc2f4502d72d0a60ba8ebcb543fb65696
oeick <oliver.eickmeyer@gmail.com>
2021-05-22 18:40:59 +0200

    added Rust solution for 2015 day 20 part 2

--- 2015/20/rust/src/main.rs
+++ 2015/20/rust/src/main.rs
@@ -4,6 +4,7 @@ fn main() -> Result<(), Box<dyn Error>>{
     let input = std::fs::read_to_string(r"..\input.txt")?;
     let presents: usize = input.parse()?;
     println!("{:?}", solve_part_1(presents));
+    println!("{:?}", solve_part_2(presents));
     Ok(())
 }
 
@@ -17,11 +18,27 @@ fn solve_part_1(presents: usize) -> usize{
     house.iter().position(|p| p >= &presents).unwrap()
 }
 
+fn solve_part_2(presents: usize) -> usize{
+    let mut house: Vec<usize> = vec![11; presents/11];
+    for elf in 2..=presents/11{
+        for delivery in (elf..presents/11).step_by(elf).take(50){
+            house[delivery] += elf * 11;
+        }
+    }
+    house.iter().position(|p| p >= &presents).unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
+
     #[test]
     fn test_part_1() {
         assert_eq!(8, solve_part_1(130usize));
+    }
+
+    #[test]
+    fn test_part_2() {
+        assert_eq!(4, solve_part_2(77usize));
     }
 }
