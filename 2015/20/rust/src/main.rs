12d9910b5687b03e310e79146f4e953a28928293
oeick <oliver.eickmeyer@gmail.com>
2021-05-22 19:35:56 +0200

    refactored Rust solution for 2015 day 20: less code duplication, more testing

--- 2015/20/rust/src/main.rs
+++ 2015/20/rust/src/main.rs
@@ -1,31 +1,26 @@
 use std::error::Error;
 
+const UNLIMITED: usize = usize::MAX;
+
 fn main() -> Result<(), Box<dyn Error>>{
     let input = std::fs::read_to_string(r"..\input.txt")?;
     let presents: usize = input.parse()?;
-    println!("{:?}", solve_part_1(presents));
-    println!("{:?}", solve_part_2(presents));
+    println!("{:?}", solve(presents, 10, UNLIMITED));
+    println!("{:?}", solve(presents, 11, 50));
     Ok(())
 }
 
-fn solve_part_1(presents: usize) -> usize{
-    let mut house: Vec<usize> = vec![10; presents/10];
-    for elf in 2..=presents/10{
-        for delivery in (elf..presents/10).step_by(elf){
-            house[delivery] += elf * 10;
+fn solve(presents: usize, capacity: usize, limit: usize) -> usize{
+    let mut house: Vec<usize> = vec![0; presents/capacity];
+    for elf in 1..=presents/capacity{
+        for delivery in (elf-1..presents/capacity).step_by(elf).take(limit){
+            house[delivery] += elf * capacity;
         }
     }
-    house.iter().position(|p| p >= &presents).unwrap()
-}
-
-fn solve_part_2(presents: usize) -> usize{
-    let mut house: Vec<usize> = vec![11; presents/11];
-    for elf in 2..=presents/11{
-        for delivery in (elf..presents/11).step_by(elf).take(50){
-            house[delivery] += elf * 11;
-        }
+    match house.iter().position(|p| p >= &presents) {
+        Some(house_number) => house_number + 1,
+        None               => 0
     }
-    house.iter().position(|p| p >= &presents).unwrap()
 }
 
 #[cfg(test)]
@@ -34,11 +29,12 @@ mod tests {
 
     #[test]
     fn test_part_1() {
-        assert_eq!(8, solve_part_1(130usize));
+        assert_eq!(8, solve(130, 10, UNLIMITED));
     }
 
     #[test]
     fn test_part_2() {
-        assert_eq!(4, solve_part_2(77usize));
+        assert_eq!(4, solve(71, 11, UNLIMITED));
+        assert_eq!(6, solve(71, 11, 3));
     }
 }
