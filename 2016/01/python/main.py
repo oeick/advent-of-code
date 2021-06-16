6346e07a2313cf68db21a211e0df3511e0894b54
oeick <oliver.eickmeyer@gmail.com>
2021-06-16 18:48:11 +0200

    refactored Python solution for 2016 day 1 (Single-responsibility principle)

--- 2016/01/python/main.py
+++ 2016/01/python/main.py
@@ -1,4 +1,4 @@
-rotations = {
+ROTATIONS = {
     'N': {'R': 'E', 'L': 'W'},
     'E': {'R': 'S', 'L': 'N'},
     'S': {'R': 'W', 'L': 'E'},
@@ -6,14 +6,14 @@ rotations = {
 }
 
 
-def solve(instructions: list[str]) -> (int, int):
+def build_path(instructions: list[str]) -> list[tuple]:
     x, y = 0, 0
     face = 'N'
     path = [(x, y)]
     for instruction in instructions:
         turn = instruction[0]
         blocks = int(instruction[1:])
-        face = rotations[face][turn]
+        face = ROTATIONS[face][turn]
         if face == 'N':
             path += [(x, y + d) for d in range(1, blocks + 1)]
         elif face == 'E':
@@ -23,12 +23,20 @@ def solve(instructions: list[str]) -> (int, int):
         else:
             path += [(x - d, y) for d in range(1, blocks + 1)]
         x, y = path[-1]
+    return path
+
+
+def find_first_location_visited_twice(path: list[tuple]) -> tuple:
     for coords in path:
         if path.count(coords) > 1:
-            break
-    else:
-        coords = 0, 0
-    return abs(x + y), sum(abs(v) for v in coords)
+            return coords
+
+
+def solve(instructions: list[str]) -> (int, int):
+    path = build_path(instructions)
+    twice = find_first_location_visited_twice(path)
+    calc_dist = lambda vec: sum(abs(x) for x in vec) if vec else -1
+    return calc_dist(path[-1]), calc_dist(twice)
 
 
 def main(filename: str) -> (int, int):
