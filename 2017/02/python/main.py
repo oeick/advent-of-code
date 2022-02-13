1c3a6034df88d38c28eea351a38e2d50d63d20cf
oeick <oliver.eickmeyer@gmail.com>
2022-02-13 19:15:03 +0100

    changed Python solution for 2017 day 2 (removed a few unnecessary brackets)

--- 2017/02/python/main.py
+++ 2017/02/python/main.py
@@ -2,12 +2,12 @@ from itertools import combinations
 
 
 def solve_part_1(rows: list[list[int]]) -> int:
-    return sum([max(row) - min(row) for row in rows])
+    return sum(max(row) - min(row) for row in rows)
 
 
 def solve_part_2(rows: list[list[int]]) -> int:
-    return sum([[max(c) // min(c) for c in combinations(row, 2)
-               if max(c) % min(c) == 0][0] for row in rows])
+    return sum([max(c) // min(c) for c in combinations(row, 2)
+                if max(c) % min(c) == 0][0] for row in rows)
 
 
 def main(filename: str) -> (int, int):
