80e04a999752cb21ad07ce0977b2409897d94f62
oeick <oliver.eickmeyer@gmail.com>
2022-10-31 21:09:06 +0100

    refactored Python solution for 2018 day 8

--- 2018/08/python/main.py
+++ 2018/08/python/main.py
@@ -1,34 +1,4 @@
-from typing import NamedTuple, Self, Optional
-
-
-class Node(NamedTuple):
-    childs: list[Self]
-    data: list[Optional[int]]
-    parent: Optional[Self]
-
-    @staticmethod
-    def of(n_childs: int, n_data: int) -> Self:
-        """ Creates new empty node with no parent."""
-        return Node([None] * n_childs, [None] * n_data, None)
-
-    def make(self, n_childs: int, n_data: int) -> Self:
-        """ Creates new node which is a child of this node."""
-        return Node([None] * n_childs, [None] * n_data, self)
-
-    def fillin_data(self, nums: list[int], i: int) -> (Self, int):
-        n_data = len(self.data)
-        for i_data, num in enumerate(nums[i:i + n_data]):
-            self.data[i_data] = num
-        return self.parent, i + n_data
-
-    def create_child(self, nums: list[int], i: int) -> (Self, int):
-        new_node = self.make(*nums[i:i + 2])
-        self.childs[len(list(filter(None, self.childs)))] = new_node
-        return new_node, i + 2
-
-    def complete(self, nums: list[int], i: int) -> (Self, int):
-        return self.fillin_data(nums, i) if all(self.childs) \
-            else self.create_child(nums, i)
+from node import Node
 
 
 def main(filename: str) -> (int, int):
