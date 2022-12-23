de1b3942f5efefcc4cbac7ffbe650a2afbf5baed
Oliver Eickmeyer <oliver.eickmeyer@gmail.com>
2022-12-23 08:13:19 +0100

    added Python solution for 2022 day 15, part 1

---
+++ 2022/15/python/main.py
@@ -0,0 +1,108 @@
+import re
+from itertools import combinations
+from typing import NamedTuple, Self
+
+PATTERN = re.compile(
+    r'Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)')
+
+
+class Coord(NamedTuple):
+    x: int
+    y: int
+
+    def distance(self, other: Self) -> int:
+        return abs(self.x - other.x) + abs(self.y - other.y)
+
+
+class Sensor(NamedTuple):
+    pos: Coord
+    beacon: Coord
+
+
+class Span(NamedTuple):
+    left: int
+    right: int
+
+    def touches(self, other: Self) -> bool:
+        return self.right >= other.left - 1 and self.left <= other.right + 1
+
+    def join(self, other: Self) -> Self:
+        return Span(min(self.left, other.left),
+                    max(self.right, other.right))
+
+    def includes(self, x: int) -> bool:
+        return self.left <= x <= self.right
+
+    def __len__(self) -> int:
+        return self.right - self.left + 1
+
+
+def main(filename: str) -> tuple[int, int]:
+    with open(filename, 'r') as fp:
+        lines = fp.read().splitlines()
+    sensors = parse_sensors(lines)
+    return solve_part_1(sensors, 2_000_000), 0
+
+
+def parse_sensors(lines: list[str]) -> list[Sensor]:
+    sensors = []
+    for line in lines:
+        match = PATTERN.match(line)
+        xs, ys, xb, yb = [int(v) for v in match.groups()[0:4]]
+        sensors.append(Sensor(Coord(xs, ys), Coord(xb, yb)))
+    return sensors
+
+
+def solve_part_1(sensors: list[Sensor], y: int) -> int:
+    exclusion_spans = get_merged_exclusion_spans(sensors, y)
+    beacons_inside_spans = get_beacons_inside_spans(
+        beacons={s.beacon for s in sensors},
+        spans=exclusion_spans,
+        y=y)
+    return sum(len(s) for s in exclusion_spans) - len(beacons_inside_spans)
+
+
+def calc_exclusion_spans(sensors: list[Sensor], y: int) -> set[Span]:
+    exclusion_spans: set[Span] = set()
+    for sensor in sensors:
+        diff = sensor.pos.distance(sensor.beacon) - abs(sensor.pos.y - y)
+        if diff < 0:
+            continue
+        exclusion_spans.add(Span(sensor.pos.x - diff, sensor.pos.x + diff))
+    return exclusion_spans
+
+
+def get_merged_exclusion_spans(sensors: list[Sensor], y: int) -> set[Span]:
+    exclusion_spans = calc_exclusion_spans(sensors, y)
+    return merge_all_overlaps(exclusion_spans)
+
+
+def get_beacons_inside_spans(
+        beacons: set[Coord], spans: set[Span], y: int) -> set[Coord]:
+    beacons_on_same_row = {b for b in beacons if b.y == y}
+    return {b for b in beacons_on_same_row
+            if any(s.includes(b.x) for s in spans)}
+
+
+def merge_first_overlap(spans: set[Span]) -> set[Span]:
+    result = set(spans)
+    for span1, span2 in combinations(spans, 2):
+        if span1.touches(span2):
+            result -= {span1, span2}
+            result.add(span1.join(span2))
+            break
+    return result
+
+
+def merge_all_overlaps(spans: set[Span]) -> set[Span]:
+    while True:
+        new_spans = merge_first_overlap(spans)
+        if len(new_spans) == len(spans):
+            return new_spans
+        spans = new_spans
+
+
+if __name__ == '__main__':
+    solution_1, solution_2 = main('../input.txt')
+    print(solution_1)
+    print(solution_2)
---
+++ 2022/15/python/tests/test_examples.py
@@ -0,0 +1,102 @@
+import unittest
+
+from parameterized import parameterized
+
+import main
+from main import Coord, Sensor, Span
+
+EXAMPLE = """
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3
+""".splitlines()[1:]
+
+
+class ExampleTests(unittest.TestCase):
+
+    def test_example_part_1(self):
+        sensors = main.parse_sensors(EXAMPLE)
+        self.assertEqual(26, main.solve_part_1(sensors, 10))
+
+    def test_solve_part_1_1_no_coord(self):
+        sensors = [Sensor(Coord(0, 0), Coord(0, 3))]
+        self.assertEqual(0, main.solve_part_1(sensors, 4))
+
+    def test_solve_part_1_1_one_coord(self):
+        sensors = [Sensor(Coord(0, 0), Coord(3, 0))]
+        self.assertEqual(1, main.solve_part_1(sensors, 3))
+
+    def test_solve_part_1_sensor_on_same_row(self):
+        sensors = [Sensor(Coord(0, 0), Coord(0, 2))]
+        self.assertEqual(5, main.solve_part_1(sensors, 0))
+
+    def test_solve_part_1_beacon_on_same_row(self):
+        sensors = [Sensor(Coord(0, 2), Coord(0, 0))]
+        self.assertEqual(0, main.solve_part_1(sensors, 0))
+
+    def test_solve_part_1_separate_spans(self):
+        sensors = [Sensor(Coord(1, 2), Coord(2, 4)),
+                   Sensor(Coord(7, 1), Coord(7, 3))]
+        self.assertEqual(6, main.solve_part_1(sensors, 0))
+
+    def test_solve_part_1_joined_spans(self):
+        sensors = [Sensor(Coord(3, 0), Coord(0, 2)),
+                   Sensor(Coord(5, -2), Coord(6, 0))]
+        self.assertEqual(5, main.solve_part_1(sensors, -4))
+
+    @parameterized.expand([
+        (False, Span(0, 2), Span(4, 6)),
+        (True, Span(0, 3), Span(4, 6)),
+        (True, Span(0, 4), Span(4, 6)),
+        (True, Span(0, 5), Span(4, 6)),
+        (True, Span(0, 6), Span(4, 6)),
+        (True, Span(0, 7), Span(4, 6)),
+        (True, Span(7, 7), Span(4, 6)),
+        (False, Span(8, 9), Span(4, 6))])
+    def test_span_touches(self, expected, span1, span2):
+        self.assertEqual(expected, span1.touches(span2))
+
+    def test_merge_first_overlap_2_to_1(self):
+        self.assertEqual({Span(-2, 5)},
+                         main.merge_first_overlap({
+                             Span(-2, 1),
+                             Span(0, 5)}))
+
+    def test_merge_first_overlap_2_to_2(self):
+        self.assertEqual({Span(-2, 1), Span(3, 5)},
+                         main.merge_first_overlap({
+                             Span(-2, 1),
+                             Span(3, 5)}))
+
+    def test_merge_all_overlaps_3_to_2(self):
+        self.assertEqual({Span(-2, 0), Span(2, 9)},
+                         main.merge_all_overlaps({
+                             Span(-2, 0),
+                             Span(2, 4),
+                             Span(3, 9)}))
+
+    def test_merge_all_overlaps_3_1(self):
+        self.assertEqual({Span(-2, 9)},
+                         main.merge_all_overlaps({
+                             Span(-2, 0),
+                             Span(1, 4),
+                             Span(2, 9)}))
+
+    def test_merge_all_overlaps_4_to_2(self):
+        self.assertEqual({Span(-2, 2), Span(4, 9)},
+                         main.merge_all_overlaps({
+                             Span(-2, 0),
+                             Span(0, 2),
+                             Span(4, 6),
+                             Span(5, 9)}))
